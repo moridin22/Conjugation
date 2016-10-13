61918ac07be98e7a976a4724e91adcedd4d3a2e2
Elliot Stahnke <elliot.stahnke@gmail.com>
2016-10-13 16:28:10 -0700

    Cleaned up code, added documentation

--- verbs.py
+++ verbs.py
@@ -1,20 +1,27 @@
 from tkinter import *
 from random import choice, randrange
 from pygame import mixer
-import codecs, configparser, sys, time
+import codecs
+import configparser
+import sys
+import time
 import tkinter.font as font
 
 class Application(Frame):
-    def __init__(self, master):
-        Frame.__init__(self, master)
-        self.grid()
-        self.create_widgets()
-        mixer.init()
-        self.alert = mixer.Sound('Funk.wav')
 
-    def create_widgets(self):
+    def __init__(self, master):
+        """Creates the structure and functionality of the application.
+
+        Args:
+        master -- the window for the Application
+        """
+        Frame.__init__(self, master)
         self.honorifics = ["Informal Low Respect", "Informal High Respect", "Formal Low Respect", "Formal High Respect"]
         self.tenses = ["Past", "Present", "Future"]
+        self.used = [""]
+        self.myfont = font.Font(family='Helvetica', size=24)
+
+        """Reads the dictionary file into lists of words."""
         self.korean_words = []
         self.english_words = []
         with codecs.open('dictionary.txt', 'r+', 'UTF-8') as f:
@@ -22,11 +29,23 @@ class Application(Frame):
                 words = line.split(":")
                 self.korean_words.append(words[0])
                 self.english_words.append(words[1].rstrip())
-            if self.korean_words[0][0]=="\ufeff": #this line is to fix a weird bug
+            """Fixes an encoding problem that occurs with UTF-8."""
+            if self.korean_words[0][0]=="\ufeff":
                 self.korean_words[0] = self.korean_words[0][1:]
-        assert len(self.korean_words) == len(self.english_words)
-        self.used = [""]
-        self.myfont = font.Font(family='Helvetica', size=24)
+        assert len(self.korean_words) == len(self.english_words), \
+               "There are a different number of korean and english words."
+
+        self.grid()
+        self.create_widgets()
+
+        """Initialize the sound."""
+        mixer.init()
+        self.alert = mixer.Sound('Funk.wav')
+
+
+    def create_widgets(self):
+        """Creates all of the necessary widgets for the application."""
+
 
         self.korean_label = Label(self, font = self.myfont)
         self.honorific_label = Label(self, font = self.myfont)
@@ -52,25 +71,26 @@ class Application(Frame):
         self.next_button.grid(row = 4, column = 0, sticky = W)
         self.count_label.grid(row = 4, column = 1, sticky = W)
 
-
-
     def beep(self):
+        """Plays a beep noise."""
         self.alert.play()
-    def next_word(self):
-        #option 1
-        """self.honorific_label["text"] = choice(self.honorifics)
-        self.tense_label["text"] = choice(self.tenses) + " Tense" """
 
-        #option 2
-        code = ""
-        while code in self.used:
+    def next_word(self):
+        """Changes the displayed word and updates the window accordingly."""
+
+        """Finds a word and conjugation pair that hasn't been used yet, then add
+        it to the list of used pairs."""
+        verb_conjugation_pair = ""
+        while verb_conjugation_pair in self.used:
             ind = randrange(0, 12)
             i = randrange(len(self.korean_words))
-            code = str(ind) + "-" + str(i)
-        self.used.append(code)
+            verb_conjugation_pair = str(ind) + "-" + str(i)
+        self.used.append(verb_conjugation_pair)
+
+        """Adds the new word, conjugation, and count, while deleting the old
+        english meaning."""
         self.honorific_label["text"] = self.honorifics[randrange(12) % 4]
         self.tense_label["text"] = self.tenses[randrange(12) // 4]
-
         self.count += 1
         self.count_label["text"] = "Words conjugated: " + str(self.count)
         self.korean_label["text"] = self.korean_words[i]
@@ -79,24 +99,23 @@ class Application(Frame):
 
 
     def show_korean(self):
+        """Shows the english translation or beeps if it's already displayed."""
         if self.english_label["text"] == "":
             self.english_label["text"] = self.english
         else:
             self.beep()
-            #sys.stdout.flush()
 
     def next_word_event(self, event):
+        """Calls the next_word method."""
         self.next_word()
 
     def show_korean_event(self, event):
+        """Calls the show_korean method."""
         self.show_korean()
 
-def raise_above_all(window):
-    window.attributes('-topmost', 1)
-    window.attributes('-topmost', 0)
 
-
-def go():
+def run():
+    """Starts the program."""
     root = Tk()
     root.title("Conjugation Time!")
     root.geometry("600x280")
@@ -104,7 +123,6 @@ def go():
     app.bind_all("n", app.next_word_event)
     app.bind_all("m", app.show_korean_event)
     root.lift()
-    raise_above_all(root)
     root.mainloop()
 
-go()
+run()
